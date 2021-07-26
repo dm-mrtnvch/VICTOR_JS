53194e0073a1836bcda3d4665e8b874d4922a578
DM-MRTNVCH <dmvshn@gmail.com>
2021-07-26 13:27:13 +0300

    class 3

--- JS Lessons/src/lessons/lesson6/lesson6.ts
+++ JS Lessons/src/lessons/lesson6/lesson6.ts
@@ -87,58 +87,180 @@ console.log('Lesson 6');
 // console.log(testObj)
 
 
-interface ITest {
-    age: number
-    sayBye: Function
-}
+// interface ITest {
+//     age: number
+//     sayBye: Function
+// }
+//
+// class Test implements ITest{
+//     name: string
+//     staticParam: number
+//
+//     static staticParam = 10
+//
+//     constructor(name: string, public age: number) {
+//         this.name = name
+//         this.age = age
+//         this.staticParam = 500
+//
+//     }
+//     sayHi() {
+//         console.log('hi from parent')
+//         console.log(this.name)
+//         console.log(Test.staticParam )
+//         console.log(this.staticParam)
+//     }
+//     sayBye() {
+//
+//     }
+//     static TestMethod() {
+//         console.log(this.staticParam)
+//     }
+// }
+//
+// let obj = new Test('raz', 1)
+//
+// class Test2 extends Test {
+//     constructor(public name: string, public age: number) {
+//         super(name, age);
+//     }
+//
+// }
+//
+// console.dir(Test2)
+//
 
-class Test implements ITest{
-    name: string
-    staticParam: number
+// interface ITest {
+//     name: string
+// }
+//
+//
+// class Test  {
+//     name: string
+//
+//     private param = 10
+//
+//     constructor(name: string) {
+//         this.name = name
+//     }
+//     sayHi() {
+//         console.log('hi from parent')
+//         console.log(this.param)
+//         this.sayByePrivate()
+//     }
+//
+//     private sayByePrivate() {
+//
+//     }
+//     sayByeNotPrivate(){
+//
+//     }
+// }
+//
+//
+// let obj = new Test('raz')
+// //@ts-ignore
+// console.log(obj.param)
+// //@ts-ignore
+// obj.sayByePrivate()
+// obj.sayByeNotPrivate()
 
-    static staticParam = 10
 
-    constructor(name: string, public age: number) {
-        this.name = name
-        this.age = age
-        this.staticParam = 500
+// class Test3 {
+//     #param = 10
+//     #testMethod(){
+//         console.log('hi')
+//     }
+//
+// }
+//
+// let testObj = new Test3()
+//
+// testObj.#param
 
-    }
-    sayHi() {
-        console.log('hi from parent')
-        console.log(this.name)
-        console.log(Test.staticParam )
-        console.log(this.staticParam)
-    }
-    sayBye() {
+// obj.sayHi ()
 
-    }
-    static TestMethod() {
-        console.log(this.staticParam)
-    }
-}
 
-let obj = new Test('raz', 1)
-
-class Test2 extends Test {
-    constructor(public name: string, public age: number) {
-        super(name, age);
-    }
-
-}
-
-console.dir(Test2)
-
-obj.sayHi ()
 // Task 01
 // Создайте структуру с именем student, содержащую поля: имя и фамилия, номер группы, успеваемость (массив из пяти элементов).
 // Создать массив из десяти элементов такого типа, упорядочить записи по возрастанию среднего балла.
 // Добавить возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 4 или 5.
 
+
+interface IStudent {
+    name: string
+    surname: string
+    groupNumber: number
+    progress: number[]
+    averageMark: number
+}
+
+class Student implements IStudent {
+    averageMark: number
+
+    constructor(public name: string, public surname: string, public groupNumber: number, public progress: Array<number>) {
+        this.name = name
+        this.surname = surname
+        this.groupNumber = groupNumber
+        this.progress = progress
+        this.averageMark = this.progress.reduce((sum, mark) => sum + mark) / this.progress.length
+    }
+
+    private static sortStudent(s1: IStudent, s2: IStudent) {
+        if (s1.averageMark > s2.averageMark) {
+            return 1
+        } else if (s1.averageMark < s2.averageMark) {
+            return -1
+        } else {
+            return 0
+        }
+    }
+
+
+    static sort(arr: IStudent[]) {
+        const temp = [...arr]
+        return temp.sort(this.sortStudent)
+
+    }
+
+    private static isAllMarksEqualCurrentMark(marks: number[], currentMark: number) {
+        return marks.every(mark => mark === currentMark)
+    }
+
+    private static filterStudents(arr: Array<IStudent>) {
+        const result: Array<IStudent> = []
+        arr.forEach(s => {
+            if (this.isAllMarksEqualCurrentMark(s.progress, 4) || this.isAllMarksEqualCurrentMark(s.progress, 5)) {
+                result.push(s)
+            }
+        })
+        return result
+    }
+
+    static printGoodStudents(arr: Array<IStudent>) {
+        this.filterStudents(arr).forEach(s => {
+            console.log(`Student - ${s.surname}, Group - ${s.groupNumber}, average - ${s.averageMark}`)
+        })
+    }
+}
+
+let students = []
+
+students.push(new Student('evgen', 'mihalich', 1, [4, 4, 4, 4]))
+students.push(new Student('ann', 'viktorovna', 2, [5, 5, 5, 5]))
+students.push(new Student('alex', 'ivanov', 3, [3, 3, 3, 3]))
+students.push(new Student('helen', 'follow', 4, [3, 4, 2, 5, 1]))
+
+
+console.log(students)
+console.log(Student.sort(students))
+Student.printGoodStudents(students)
+
 // Task 02
 // Создать класс с двумя переменными. Добавить конструктор с входными параметрами и инициализирующий члены класса по умолчанию.
 // Можно ли создать метод на экземпляре класса который будет удалять сам экземпляр класса?
 // Можно ли создать метод класса который будет удалять экземпляр класса?
+
 
 // Task 03
 // Составить описание класса для представления времени. Предусмотреть возможности установки времени и изменения его отдельных
